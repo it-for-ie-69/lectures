5e06b4703a70a3724c623c7f04cb3ff5d784dd9b
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-07-24 09:16:36 +0700

    Update

--- build.ps1
+++ build.ps1
@@ -30,6 +30,6 @@ $folder = "src\T08_vue"
 npm run html $folder
 npm run pdf $folder
 
-# $folder = "src\T09_vue_basics"
-# npm run html $folder
-# npm run pdf $folder
+$folder = "src\T09_vue_basics"
+npm run html $folder
+npm run pdf $folder
